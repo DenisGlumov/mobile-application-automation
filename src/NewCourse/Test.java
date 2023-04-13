1f53124ff5554f527f8fe00b9b2e9b9dc0400492
glumovdenis <den.glumov@gmail.com>
2023-04-13 10:58:01 +0700

    тест

--- src/NewCourse/Test.java
+++ src/NewCourse/Test.java
@@ -3,7 +3,7 @@ package src.NewCourse;
 public class Test {
     public static void main(String[] args) {
         for (int j = 0; j < 10; j++) {
-            System.out.print('*');
+            System.out.print('-');
         }
         System.out.println();
     }
